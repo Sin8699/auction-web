d01d1fe9ae24b133dbe4f51217f06c65f9a001dd
thanhdinh <dinhcongthanh1910@gmail.com>
2021-10-11 14:19:52 +0700

    clear

--- src/App.js
+++ src/App.js
@@ -1,95 +1,64 @@
-/**
-=========================================================
-* Soft UI Dashboard Material-UI - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-material-ui
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-import { useState, useEffect } from "react";
-
-// react-router components
-import { Route, Switch, Redirect, useLocation } from "react-router-dom";
-
-// jss components
-import { create } from "jss";
-
-// @material-ui core components
-import { ThemeProvider, StylesProvider, jssPreset } from "@material-ui/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Icon from "@material-ui/core/Icon";
-
-// Soft UI Dashboard Material-UI components
-import SuiBox from "components/SuiBox";
-
-// Soft UI Dashboard Material-UI example components
-import Sidenav from "component-pages/Sidenav";
-import Configurator from "component-pages/Configurator";
-
-// Soft UI Dashboard Material-UI themes
-import theme from "assets/theme";
-
-// Soft UI Dashboard Material-UI routes
-import routes from "routes";
-
-// Soft UI Dashboard Material-UI contexts
-import { useSoftUIController } from "context";
+import {useState, useEffect} from 'react'
+import {Route, Switch, Redirect, useLocation} from 'react-router-dom'
+import {create} from 'jss'
+import {ThemeProvider, StylesProvider, jssPreset} from '@material-ui/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Icon from '@material-ui/core/Icon'
+import SuiBox from 'components/SuiBox'
+import Sidenav from 'component-pages/Sidenav'
+import Configurator from 'component-pages/Configurator'
+import theme from 'assets/theme'
+import routes from 'routes'
+import {useSoftUIController} from 'context'
 
 export default function App() {
-  const [controller, dispatch] = useSoftUIController();
-  const { miniSidenav, layout, openConfigurator } = controller;
-  const [onMouseEnter, setOnMouseEnter] = useState(false);
-  const { pathname } = useLocation();
+  const [controller, dispatch] = useSoftUIController()
+  const {miniSidenav, layout, openConfigurator} = controller
+  const [onMouseEnter, setOnMouseEnter] = useState(false)
+  const {pathname} = useLocation()
 
-  // JSS presets for the rtl
-  const jss = create({ plugins: [...jssPreset().plugins] });
+  // JSS presets
+  const jss = create({plugins: [...jssPreset().plugins]})
 
   // Open sidenav when mouse enter on mini sidenav
   const handleOnMouseEnter = () => {
     if (miniSidenav && !onMouseEnter) {
-      dispatch({ type: "MINI_SIDENAV", value: false });
-      setOnMouseEnter(true);
+      dispatch({type: 'MINI_SIDENAV', value: false})
+      setOnMouseEnter(true)
     }
-  };
+  }
 
   // Close sidenav when mouse leave mini sidenav
   const handleOnMouseLeave = () => {
     if (onMouseEnter) {
-      dispatch({ type: "MINI_SIDENAV", value: true });
-      setOnMouseEnter(false);
+      dispatch({type: 'MINI_SIDENAV', value: true})
+      setOnMouseEnter(false)
     }
-  };
+  }
 
   // Change the openConfigurator state
   const handleConfiguratorOpen = () => {
-    dispatch({ type: "OPEN_CONFIGURATOR", value: !openConfigurator });
-  };
+    dispatch({type: 'OPEN_CONFIGURATOR', value: !openConfigurator})
+  }
 
   // Setting page scroll to 0 when changing the route
   useEffect(() => {
-    document.documentElement.scrollTop = 0;
-    document.scrollingElement.scrollTop = 0;
-  }, [pathname]);
+    document.documentElement.scrollTop = 0
+    document.scrollingElement.scrollTop = 0
+  }, [pathname])
 
   const getRoutes = (allRoutes) =>
     allRoutes.map((route) => {
       if (route.collapse) {
-        return getRoutes(route.collapse);
+        return getRoutes(route.collapse)
       }
 
       if (route.route) {
-        return <Route exact path={route.route} component={route.component} key={route.key} />;
+        return <Route exact path={route.route} component={route.component} key={route.key} />
       }
 
-      return null;
-    });
+      return null
+    })
 
   const configsButton = (
     <SuiBox
@@ -112,13 +81,13 @@ export default function App() {
         settings
       </Icon>
     </SuiBox>
-  );
+  )
 
   return (
     <StylesProvider jss={jss}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        {layout === "dashboard" && (
+        {layout === 'dashboard' && (
           <>
             <Sidenav
               routes={routes}
@@ -129,12 +98,12 @@ export default function App() {
             {configsButton}
           </>
         )}
-        {layout === "vr" && <Configurator />}
+        {layout === 'vr' && <Configurator />}
         <Switch>
           {getRoutes(routes)}
-          <Redirect from="*" to="/dashboards/default" />
+          <Redirect from="*" to="/dashboard" />
         </Switch>
       </ThemeProvider>
     </StylesProvider>
-  );
+  )
 }
