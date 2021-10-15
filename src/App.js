8dede389680d741b1e67d513f2d8f98cbdd0a5bc
thanhdinh <dinhcongthanh1910@gmail.com>
2021-10-16 05:39:52 +0700

    remove onMouseEnter and onMouseLeave and functions in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import { Route, Switch, Redirect, useLocation } from 'react-router-dom'
 import { create } from 'jss'
 import { ThemeProvider, StylesProvider, jssPreset } from '@material-ui/styles'
@@ -6,35 +6,13 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import Sidenav from 'component-pages/Sidenav'
 import theme from 'assets/theme'
 import routes from 'routes'
-import { useSoftUIController } from 'context'
 
 export default function App() {
-  const [controller, dispatch] = useSoftUIController()
-  const { miniSidenav, openConfigurator } = controller
-  // const { miniSidenav } = controller
-  const [onMouseEnter, setOnMouseEnter] = useState(false)
   const { pathname } = useLocation()
 
   // JSS presets
   const jss = create({ plugins: [...jssPreset().plugins] })
 
-  // // Open sidenav when mouse enter on mini sidenav
-  const handleOnMouseEnter = () => {
-    if (miniSidenav && !onMouseEnter) {
-      dispatch({ type: 'MINI_SIDENAV', value: false })
-      setOnMouseEnter(true)
-    }
-  }
-
-  // // Close sidenav when mouse leave mini sidenav
-  const handleOnMouseLeave = () => {
-    if (onMouseEnter) {
-      dispatch({ type: 'MINI_SIDENAV', value: true })
-      setOnMouseEnter(false)
-    }
-  }
-
-  // Setting page scroll to 0 when changing the route
   useEffect(() => {
     document.documentElement.scrollTop = 0
     document.scrollingElement.scrollTop = 0
@@ -55,12 +33,7 @@ export default function App() {
     <StylesProvider jss={jss}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Sidenav
-          routes={routes}
-          onMouseEnter={handleOnMouseEnter}
-          onMouseLeave={handleOnMouseLeave}
-        />
-
+        <Sidenav routes={routes} />
         <Switch>
           {getRoutes(routes)}
           <Redirect from="*" to="/dashboard" />
