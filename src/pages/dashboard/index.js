788d48a54d27fcd1e14c16f21eb462ebb5472455
Phuong <daoanhphuong080699@gmail.com>
2021-10-28 20:30:41 +0700

    update category menu

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -17,9 +17,11 @@ import team1 from 'assets/images/team-1.jpg'
 import Card from '@material-ui/core/Card'
 import SuiButton from 'components/SuiButton'
 import Icon from '@material-ui/core/Icon'
-import Menu from '@material-ui/core/Menu'
+import { Menu, MenuItem, SubMenu } from '@szhsin/react-menu'
+import '@szhsin/react-menu/dist/index.css'
+import '@szhsin/react-menu/dist/transitions/slide.css'
+
 import CircularProgress from '@material-ui/core/CircularProgress'
-import MenuItem from '@material-ui/core/MenuItem'
 import { useState } from 'react'
 import SuiPagination from '../../components/SuiPagination/index'
 import SuiInput from '../../components/SuiInput/index'
@@ -30,14 +32,9 @@ import keyBy from 'lodash/keyBy'
 import BidModal from './components/BidModal/index'
 
 function Dashboard() {
-  const [openMenu, setOpenMenu] = useState(null)
-
   const [{ data: products, loading: loadingProducts, error: errorProducts }] = useGetProducts()
   const [{ data: biddingProducts, loading: loadingBiddingProducts, error: errorBiddingProducts }] =
     useGetBiddingProducts()
-
-  const handleOpenMenu = ({ currentTarget }) => setOpenMenu(currentTarget)
-  const handleCloseMenu = () => setOpenMenu(null)
 
   const _renderData = () => {
     if (loadingProducts || loadingBiddingProducts) {
@@ -94,20 +91,25 @@ function Dashboard() {
                 <SuiInput withIcon={{ icon: 'search', direction: 'right' }} placeholder="Search" />
               </SuiBox>
               <>
-                <SuiButton variant="gradient" buttonColor="secondary" onClick={handleOpenMenu}>
-                  Category
-                  <Icon className="material-icons-round font-bold">keyboard_arrow_down</Icon>
-                </SuiButton>
                 <Menu
-                  transformOrigin={{ vertical: 'top', horizontal: 'left' }}
-                  anchorEl={openMenu}
-                  getContentAnchorEl={null}
-                  open={Boolean(openMenu)}
-                  onClose={handleCloseMenu}
+                  menuButton={
+                    <SuiButton variant="gradient" buttonColor="secondary">
+                      Category
+                      <Icon className="material-icons-round font-bold">keyboard_arrow_down</Icon>
+                    </SuiButton>
+                  }
                 >
-                  <MenuItem onClick={handleCloseMenu}>Electronics</MenuItem>
-                  <MenuItem onClick={handleCloseMenu}>Cloting</MenuItem>
-                  <MenuItem onClick={handleCloseMenu}>Shoes</MenuItem>
+                  <MenuItem>New File</MenuItem>
+                  <SubMenu label="Open">
+                    <MenuItem>index.html</MenuItem>
+                    <MenuItem>example.js</MenuItem>
+                    <SubMenu label="Styles">
+                      <MenuItem>about.css</MenuItem>
+                      <MenuItem>home.css</MenuItem>
+                      <MenuItem>index.css</MenuItem>
+                    </SubMenu>
+                  </SubMenu>
+                  <MenuItem>Save</MenuItem>
                 </Menu>
               </>
             </SuiBox>
