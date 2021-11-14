4368f9e86cb9d0556a08c6907a0f4bc102e61487
Hieu Hoang <hieuhoang@kobiton.com>
2021-11-15 02:19:59 +0700

    update common components

---
+++ src/component-pages/SuiButton/index.js
@@ -0,0 +1,63 @@
+import {forwardRef} from 'react'
+import PropTypes from 'prop-types'
+import clsx from 'clsx'
+
+// @material-ui core components
+import Button from '@material-ui/core/Button'
+
+// Custom styles for SuiButton
+import styles from 'components/SuiButton/styles'
+
+const SuiButton = forwardRef(
+  ({buttonColor, variant, size, circular, iconOnly, children, customClass, ...rest}, ref) => {
+    const classes = styles({buttonColor, variant, size, iconOnly})
+
+    return (
+      <Button
+        {...rest}
+        ref={ref}
+        variant={variant === 'gradient' ? 'contained' : variant}
+        size={size}
+        className={clsx(classes[variant], customClass, {
+          [classes.circular]: circular,
+          [classes.iconOnly]: iconOnly
+        })}
+      >
+        {children}
+      </Button>
+    )
+  }
+)
+
+// Setting default values for the props of SuiButton
+SuiButton.defaultProps = {
+  size: 'medium',
+  variant: 'contained',
+  buttonColor: 'white',
+  circular: false,
+  iconOnly: false,
+  customClass: ''
+}
+
+// Typechecking props for the SuiButton
+SuiButton.propTypes = {
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  variant: PropTypes.oneOf(['text', 'contained', 'outlined', 'gradient']),
+  buttonColor: PropTypes.oneOf([
+    'white',
+    'primary',
+    'secondary',
+    'info',
+    'success',
+    'warning',
+    'error',
+    'light',
+    'dark'
+  ]),
+  circular: PropTypes.bool,
+  iconOnly: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+  customClass: PropTypes.string
+}
+
+export default SuiButton
